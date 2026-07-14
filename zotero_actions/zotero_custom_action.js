ff5eb236c130f30e6d608d9df878877199edce5d
xiahong <xiahahaha01@gmail.com>
2026-07-14 10:29:59 +0800

    fix: format

--- zotero_actions/zotero_custom_action.js
+++ zotero_actions/zotero_custom_action.js
@@ -7,10 +7,10 @@ if (!item) return "No item selected";
 // 引入你的外部真实业务脚本 (假设存放于特定目录或通过网络请求获取)
 var scriptUrl = "file:///C:/Users/YourName/ZoteroScripts/my_real_script.js";
 var request = new XMLHttpRequest();
-request.open('GET', scriptUrl, false);
+request.open("GET", scriptUrl, false);
 request.send(null);
 
 if (request.status === 200) {
-    // 执行读取到的外部代码
-    eval(request.responseText);
+  // 执行读取到的外部代码
+  eval(request.responseText);
 }
